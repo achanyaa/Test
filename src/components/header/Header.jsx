a97e0bc85231a7a08b1655d51a3c50c4d9d55eff
HariReact <hari.murali>
2024-05-14 16:47:33 -0700

    changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import { GiHamburgerMenu } from "react-icons/gi";
 const Header = ({ toggleSidebar }) => {
   return (
     <header className="header">
-      <button className="menu-btn" onClick={toggleSidebar}>Menu</button>
+      <button className="menu-btn" onClick={toggleSidebar}><GiHamburgerMenu/></button>
       <h1>Custom Material UI Components</h1>
     </header>
   );
